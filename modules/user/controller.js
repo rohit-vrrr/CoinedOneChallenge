b809beb9a0cee702d9fa16f3c364215a47490857
Rohit <vr.rohit03@gmail.com>
2021-10-26 20:23:14 +0530

    updated code

--- modules/user/controller.js
+++ modules/user/controller.js
@@ -6,18 +6,13 @@ const userMiddleware = require("../../middleware/user.js");
 const router = express.Router();
 
 /**
- * Add new user work time
+ * User registration
  * Middleware - Checking for existing users with same name
  */
-router.post('/addWorkTime', userMiddleware.checkExistingUser, (req, res) => {
+router.post('/userRegister', userMiddleware.checkExistingUser, (req, res) => {
 
     let obj = {
-        name: _.toLower(req.body.name),
-        workDays: [{
-            daysActive: req.body.workDays.daysActive,
-            from: req.body.workDays.from,
-            to: req.body.workDays.to
-        }]
+        name: _.toLower(req.body.name)
     };
     let model = new User.UserModel(obj);
 
@@ -29,51 +24,18 @@ router.post('/addWorkTime', userMiddleware.checkExistingUser, (req, res) => {
             res.status(200).json({
                 success: true,
                 id: user._id,
-                message: "Work Time Added Successfully"
+                message: "User Registered Successfully"
             });
         }
     });
 });
 
 /**
- * Fetching all the users
- */
-router.get('/:name', (req, res) => {
-
-    let name = _.toLower(req.params.name);
-
-    User.UserModel.findOne({ name }, (err, user) => {
-
-        if(err) {
-            res.status(400).send(err.message);
-        } else {
-
-            if(user) {
-                if(user.name === name) {
-                    res.status(200).send({
-                        success: true,
-                        userDetails: {
-                            name: _.capitalize(user.name),
-                            workDays: user.workDays
-                        }
-                    });
-                } else {
-                    res.status(400).send({
-                        success: false,
-                        message: "User Doesn't Exist"
-                    });
-                }
-            }
-        }
-    })
-});
-
-/**
- * Adding more work time
+ * Add work time
  * Middleware1 - Checking for time overlaps w.r.t. day & time
  * Middleware2 - Check number of entries (to not exceed 5)
  */
-router.put('/updateWorkTime/:name',
+router.put('/addWorkTime/:name',
     userMiddleware.checkTimeOverlap,
     userMiddleware.checkWorkTimeLimit,
     (req, res) => {
@@ -92,6 +54,209 @@ router.put('/updateWorkTime/:name',
                     message: "User Work Time Updated"
                 });
             }
+    });
+});
+
+/**
+ * Add blocked apps
+ */
+router.put('/blockApps/:name', (req, res) => {
+
+    let name = req.params.name;
+
+    User.UserModel.findOneAndUpdate(
+        { name },
+        { blockedApps: req.body.blockedApps }, (err, user) => {
+
+            if(err) {
+                res.status(400).send(err.message);
+            } else {
+
+                if(user) {
+                    res.status(200).send({
+                        success: true,
+                        message: "Blocked Apps Added Successfully"
+                    });
+                } else {
+                    res.status(404).send({
+                        success: false,
+                        message: "User Doesn't Exist"
+                    });
+                }
+            }
+        }
+    );
+});
+
+/**
+ * Add limited Apps
+ */
+router.put('/limitedApps/:name', (req, res) => {
+
+    let name = req.params.name;
+
+    User.UserModel.findOneAndUpdate(
+        { name },
+        { $push: { limitedApps: req.body.limitedApps } }, (err, data) => {
+
+            if(err) {
+                res.status(400).send(err.message);
+            } else {
+                res.status(200).send({
+                    success: true,
+                    message: "Limited Apps Added Successfully"
+                })
+            }
+        }
+    );
+});
+
+/**
+ * Fetch all the user details
+ */
+router.get('/:name/getAll', (req, res) => {
+
+    let name = _.toLower(req.params.name);
+
+    User.UserModel.findOne({ name }, (err, user) => {
+
+        if(err) {
+            res.status(400).send(err.message);
+        } else {
+
+            if(user) {
+                if(user.name === name) {
+                    res.status(200).send({
+                        success: true,
+                        userDetails: {
+                            name: _.capitalize(user.name),
+                            workDays: user.workDays,
+                            blockedApps: user.blockedApps,
+                            limitedAps: user.limitedApps
+                        }
+                    });
+                } else {
+                    res.status(400).send({
+                        success: false,
+                        message: "User Doesn't Exist"
+                    });
+                }
+            }
+        }
+    })
+});
+
+/**
+ * Fetch user work days
+ */
+ router.get('/:name/getWorkDays', (req, res) => {
+
+    let name = _.toLower(req.params.name);
+
+    User.UserModel.findOne({ name }, (err, user) => {
+
+        if(err) {
+            res.status(400).send(err.message);
+        } else {
+
+            if(user) {
+                if(user.name === name) {
+                    User.UserModel.find({}, { workDays: 1, _id: 0 }, (e, data) => {
+                        if(e) {
+                            res.status(400).send(e.message);
+                        } else {
+                            data.map((item) => {
+                                
+                                res.status(200).send({
+                                    workDays: item.workDays
+                                });
+                            });
+                        }
+                    });
+                }
+            } else {
+                res.status(400).send({
+                    success: false,
+                    message: "User Doesn't Exist"
+                });
+            }
+        }
+    });
+});
+
+/**
+ * Fetch user blocked apps
+ */
+router.get('/:name/getBlockedApps', (req, res) => {
+
+    let name = _.toLower(req.params.name);
+
+    User.UserModel.findOne({ name }, (err, user) => {
+
+        if(err) {
+            res.status(400).send(err.message);
+        } else {
+
+            if(user) {
+                if(user.name === name) {
+                    User.UserModel.find({}, { blockedApps: 1, _id: 0 }, (e, data) => {
+                        if(e) {
+                            res.status(400).send(e.message);
+                        } else {
+                            data.map((item) => {
+                                
+                                res.status(200).send({
+                                    blockedApps: item.blockedApps
+                                });
+                            });
+                        }
+                    });
+                }
+            } else {
+                res.status(400).send({
+                    success: false,
+                    message: "User Doesn't Exist"
+                });
+            }
+        }
+    });
+});
+
+/**
+ * Fetch user limited apps 
+ */
+router.get('/:name/getLimitedApps', (req, res) => {
+
+    let name = _.toLower(req.params.name);
+
+    User.UserModel.findOne({ name }, (err, user) => {
+
+        if(err) {
+            res.status(400).send(err.message);
+        } else {
+
+            if(user) {
+                if(user.name === name) {
+                    User.UserModel.find({}, { limitedApps: 1, _id: 0 }, (e, data) => {
+                        if(e) {
+                            res.status(400).send(e.message);
+                        } else {
+                            data.map((item) => {
+                                
+                                res.status(200).send({
+                                    limitedApps: item.limitedApps
+                                });
+                            });
+                        }
+                    });
+                }
+            } else {
+                res.status(400).send({
+                    success: false,
+                    message: "User Doesn't Exist"
+                });
+            }
+        }
     });
 });
 
--- modules/user/model.js
+++ modules/user/model.js
@@ -4,14 +4,28 @@ const user = {
     name: String,
     workDays: [
         {
+            _id: false,
             daysActive: {
                 type: [String],
-                enum: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+                enum: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+                required: true
             },
             from: String,
             to: String
         }
-    ]
+    ],
+    blockedApps: { type:[String] },
+    limitedApps: {
+        weekdays: {
+            app: { type: String },
+            limit: { type: String }
+        },
+        weekends: {
+            app: { type: String },
+            limit: { type: String }
+        }
+    }
+
 }
 
 const userSchema = new mongoose.Schema(user, { versionKey: false, timestamps: true });
